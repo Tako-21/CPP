52f26ea8899357570c7e43899081ee20fc8c3397
Tako <m.bbird21@proton.me>
2023-09-21 17:39:35 +0200

    ifstream && ofstream done

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -5,32 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmeguedm <mmeguedm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/21 11:28:19 by mmeguedm          #+#    #+#             */
-/*   Updated: 2023/09/21 17:00:49 by mmeguedm         ###   ########.fr       */
+/*   Created: 2023/09/21 17:07:25 by mmeguedm          #+#    #+#             */
+/*   Updated: 2023/09/21 17:37:37 by mmeguedm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include "Weapon.hpp"
-#include "HumanA.hpp"
-#include "HumanB.hpp"
+# include <iostream>
+# include <fstream>
+# include <string>
 
 int	main( void )
 {
-	{
-	Weapon club = Weapon("crude spiked club");
-	HumanA bob("Bob", club);
-	bob.attack();
-	club.setType("some other type of club");
-	bob.attack();
+	std::ifstream	infile ("file");
+	std::ofstream	outfile ("outfile");
+	std::string		str;
+
+	if ( infile.is_open() ) {
+		while ( infile ) {
+			std::getline(infile, str);
+			outfile << str << std::endl;
+			std::cout << str << std::endl;
+		}
 	}
-	{
-	Weapon club = Weapon("crude spiked club");
-	HumanB jim("Jim");
-	jim.setWeapon(club);
-	jim.attack();
-	club.setType("some other type of club");
-	jim.attack();
-	}
-return 0;
+
 }
