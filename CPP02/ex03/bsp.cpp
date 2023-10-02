0d99cef57c0e067c9dd906699eb61edecbac0f6c
Tako <m.bbird21@proton.me>
2023-10-02 23:58:17 +0200

    bsp done

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -6,29 +6,16 @@
 /*   By: mmeguedm <mmeguedm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:46:49 by mmeguedm          #+#    #+#             */
-/*   Updated: 2023/10/02 23:41:06 by mmeguedm         ###   ########.fr       */
+/*   Updated: 2023/10/02 23:53:49 by mmeguedm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include "Point.hpp"
 
-// float	vectorialProduct( Point const a, Point const b, Point const c )
-// {
-// 	return ( (b.getx() - a.getx()) * (c.gety() - a.gety())) - ((b.gety() - a.gety()) * (c.gety() - a.gety()) );
-// }
-
 float	vectorialProduct( Point const a, Point const b, Point const point )
 {
 	return ( ((a.getx() - point.getx()) * (b.gety() - point.gety())) - ((a.gety() - point.gety()) * (b.getx() - point.getx())) );
-}
-
-
-bool	isInSegment( Point const a, Point const b, Point const point )
-{
-	if ( ((b.getx() - a.getx()) * (point.gety() - a.gety())) - ((b.gety() - a.gety()) * (point.gety() - a.gety())) == 0)
-		return (true);
-	return (false);
 }
 
 bool	bsp( Point const a, Point const b, Point const c, Point const point)
@@ -37,13 +24,10 @@ bool	bsp( Point const a, Point const b, Point const c, Point const point)
 	int	i = 1;
 	int	j = 0;
 
-	// if (((c.getx() - point.getx()) * (a.gety() - point.gety())) - ((c.gety() - point.gety()) * (a.getx() - point.getx())) > 0)
-	// 	tab[1] = 1;
-	// if (((a.getx() - point.getx()) * (b.gety() - point.gety())) - ((a.gety() - point.gety()) * (b.getx() - point.getx())) > 0)
-	// 	tab[2] = 1;
-	// if (((b.getx() - point.getx()) * (c.gety() - point.gety())) - ((b.gety() - point.gety()) * (c.getx() - point.getx())) > 0)
-	// 	tab[3] = 1;
-	
+	if (!vectorialProduct(a, b, c)) {
+		std::cout << "It's not a triangle !" << std::endl;
+		return (false);
+	}
 	if ( vectorialProduct(c, a, point) > 0)
 		tab[1] = 1;
 	if ( vectorialProduct(a, b, point) > 0)
@@ -52,26 +36,12 @@ bool	bsp( Point const a, Point const b, Point const c, Point const point)
 		tab[1] = 1;
 	for (; tab[i] == 0 && i < 3; i++);
 	for (; tab[j] == 0 && j < 3; j++);
-	// for (int i = 0; i < 3; i++){
-	// 	std::cout << tab[i] << std::endl;
-	// }
-	// if ( i == 3	|| j == 3
-	// 			&& isInSegment(a, b, point)
-	// 			&& isInSegment(a, c, point)
-	// 			&& isInSegment(b, c, point)
-	// 			&& isInSegment(a, b, c))
-	if (i == 3 || j == 3)
-		return (true);	
-	return (false);
-	// std::cout << a.gety() << std::endl;
 	
-	// std::cout << b.getx() << std::endl;
-	// std::cout << b.gety() << std::endl;
-	
-	// std::cout << c.getx() << std::endl;
-	// std::cout << c.gety() << std::endl;
-	
-	// std::cout << point.getx() << std::endl;
-	// std::cout << point.gety() << std::endl;
-
+	if (i != 3 || j != 3)
+		return (false);
+	if (		!vectorialProduct(a, b, point)
+			||	!vectorialProduct(a, c, point)
+			||	!vectorialProduct(b, c, point)
+		return (false);
+	return (true);
 }
--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mmeguedm <mmeguedm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 12:00:56 by mmeguedm          #+#    #+#             */
-/*   Updated: 2023/10/02 23:41:12 by mmeguedm         ###   ########.fr       */
+/*   Updated: 2023/10/02 23:55:47 by mmeguedm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 
 int	main( void )
 {
-	Point	a(1.0f, 2.0f), b(7.0f, 2.0f), c(1.0f, -3.0f), point(1.0f, 0.0f);
+	Point	a(1.0f, 2.0f), b(7.0f, 2.0f), c(2.0f, 2.0f), point(2.0f, 2.0f);
 
 	std::cout << "Main Raw Bits : " << a.getx() << std::endl;
 	if (bsp(a, b, c, point))
 		std::cout << "Is in the triangle !" << std::endl;
 	else
-		std::cout << "Is not in the triangle" << std::endl;
+		std::cout << "Is not in the triangle." << std::endl;
 	return 0;
 }
