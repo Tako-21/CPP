5c7f5f923a64fcf2cd1154f87a555b1dc9ee2c9e
Tako <m.bbird21@proton.me>
2023-12-04 15:41:42 +0100

    jacob done

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: mmeguedm <mmeguedm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 17:37:39 by mmeguedm          #+#    #+#             */
-/*   Updated: 2023/12/03 22:38:29 by mmeguedm         ###   ########.fr       */
+/*   Updated: 2023/12/04 15:41:33 by mmeguedm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,11 +72,7 @@ void	insertPair( std::vector<int> & johnson, unsigned element_to_insert, unsigne
 
 	for ( unsigned i = 0; i < index_pair; it++, i++ ) {}
 	
-	std::cout << "INSERTION" << " elem : " << element_to_insert << std::endl;
 	johnson.insert(it, element_to_insert);
-	displayVec(johnson);
-	vec = johnson;
-	std::cout << "END" << std::endl;
 }
 
 unsigned long	comparePair( std::vector<int> johnson, const unsigned long number_to_compare, const unsigned range )
@@ -87,7 +83,6 @@ unsigned long	comparePair( std::vector<int> johnson, const unsigned long number_
 	std::cout << "range : " << range << std::endl;
 
 	std::cout << "JOHNSON COMPARE" << std::endl;
-	displayVec(johnson);
 	while ( i < johnson.size() ) {
 		if ( number_to_compare < johnson[i])
 			return ( index_pair * range );
@@ -97,7 +92,7 @@ unsigned long	comparePair( std::vector<int> johnson, const unsigned long number_
 	return (0);
 }
 
-void	jacobsthal( std::vector<int> & johnson, std::vector<int> & odd, std::vector<int> vec, const int step, const int pair )
+void	jacobsthal( std::vector<int> & johnson, std::vector<int> & odd, std::vector<int> & vec, const int step, const int pair )
 {
 	static const int	jacob[] = {0, 1, 3, 5, 11, 21, 43, 85, 171, 341, 683, 1365, 2731, 5461, 10923, 21845};
 	std::vector<int>::iterator	it = johnson.begin();
@@ -106,6 +101,7 @@ void	jacobsthal( std::vector<int> & johnson, std::vector<int> & odd, std::vector
 
 	unsigned	range_jacob;
 	
+	std::cout << "------STEP : " << step << "---------" << std::endl;
 	range_jacob = (step >> 1);
 	int	i = 0;
 	int	j = 0;
@@ -138,18 +134,24 @@ void	jacobsthal( std::vector<int> & johnson, std::vector<int> & odd, std::vector
 			for ( int i = 0; i < range_jacob; i++ ) {
 				index_pair = j * step + i;
 			}
-			std::cout << vec[index_pair] << std::endl;
 			index_to_insert = comparePair(johnson, vec[index_pair], range_jacob);
 			for ( int i = 0; i < range_jacob; i++ ) {
 				index_pair = j * step + i;
-				std::cout << "index_to_insert : " << index_to_insert << std::endl;
 				insertPair( johnson, vec[index_pair], index_to_insert + i, vec );
 			}
-			std::cout << "index_to_insert : " << index_to_insert << std::endl;
-			//insertion de la petite partie de la paire a l indice j
-			//pour se faire, trouver avec le binary search ou inserer la petite partie de la paire
 			j--;
 		}
+	}
+	int	k = 0;
+	i = 0;
+	
+	while ( i + range_jacob <= odd.size() ) {
+		index_to_insert = comparePair(johnson, odd[index_pair], range_jacob);
+		// for (int j = 0, k = i; j < range_jacob; ++j, ++k ) {
+		
+		// }
+		i += range_jacob << 1;
+	
 	}
 	vec = johnson;
 	displayVec( vec );
--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: mmeguedm <mmeguedm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 17:37:31 by mmeguedm          #+#    #+#             */
-/*   Updated: 2023/12/03 21:47:24 by mmeguedm         ###   ########.fr       */
+/*   Updated: 2023/12/04 14:55:42 by mmeguedm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,5 @@ unsigned long	getSize( std::vector<int>::iterator it, std::vector<int>::iterator
 void			swapPair( std::vector<int>::iterator it, std::vector<int>::iterator end );
 void			insertPair( std::vector<int>::iterator it, std::vector<int>::iterator end, std::vector<int> & johnson );
 void			fordJohson( std::vector<int>& vec, std::vector<int> & odd, int step );
-void			jacobsthal( std::vector<int> & johnson, std::vector<int> & odd, std::vector<int> vec, const int step, const int pair );
+void			jacobsthal( std::vector<int> & johnson, std::vector<int> & odd, std::vector<int> & vec, const int step, const int pair );
 unsigned long	comparePair( std::vector<int> johnson, const unsigned long number_to_compare, const unsigned range );
